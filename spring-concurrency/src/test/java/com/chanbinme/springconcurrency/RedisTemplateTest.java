373252b1a5f9bf3e6cabf322a69637940ab326e4
chanbinme <gksmfcksqls@gmail.com>
2025-08-28 23:27:57 +0900

    test: Redis Repository 테스트

--- spring-concurrency/src/test/java/com/chanbinme/springconcurrency/RedisTemplateTest.java
+++ spring-concurrency/src/test/java/com/chanbinme/springconcurrency/RedisTemplateTest.java
@@ -1,5 +1,7 @@
 package com.chanbinme.springconcurrency;
 
+import com.chanbinme.springconcurrency.redisentity.SampleObject;
+import com.chanbinme.springconcurrency.repository.SampleObjectRepository;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,9 @@ public class RedisTemplateTest {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private SampleObjectRepository sampleObjectRepository;
 
     @Test
     void testRedisTemplate() {
@@ -32,5 +37,29 @@ public class RedisTemplateTest {
         assert retrievedMap.get("b").equals(2);
 
         System.out.println("Retrieved Map: " + retrievedMap);
+    }
+
+    @Test
+    void redisRepositoryTest() throws InterruptedException {
+        // SampleObject 저장 및 조회
+        SampleObject sampleObject = SampleObject.builder()
+            .id(1L)
+            .field1("testName")
+            .field2(42)
+            .field3(true)
+            .ttlSeconds(1L) // 1 seconds TTL
+            .build();
+        SampleObject obj = sampleObjectRepository.save(sampleObject);
+        SampleObject retrievedObj = sampleObjectRepository.findById(obj.id()).orElseThrow();
+
+        assert "testName".equals(retrievedObj.field1());
+
+        System.out.println("Retrieved Object: " + retrievedObj);
+
+        Thread.sleep(1000); // Wait for 1 second to ensure TTL expiration
+
+        // 조회하면 예외 발생
+        boolean exists = sampleObjectRepository.findById(obj.id()).isPresent();
+        assert !exists;
     }
 }
