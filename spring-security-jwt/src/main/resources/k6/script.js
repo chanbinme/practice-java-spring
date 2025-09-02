7616e89277f3ea562dc3637c6bb25ce237e14ac3
chanbinme <gksmfcksqls@gmail.com>
2025-09-02 23:36:06 +0900

    feat: k6 + grafana + influxDB + docker를 통한 부하테스트 구현

---
+++ spring-security-jwt/src/main/resources/k6/script.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    vus: 10,
+    iterations: 100,
+    duration: '30s',
+}
+
+export default function () {
+    const url = 'http://192.168.45.186:8080/api/auth/login';
+    const payload = JSON.stringify({
+        username: 'user',
+        password: 'password',
+    });
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+    const res = http.post(url, payload, params);
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
